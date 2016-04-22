4c3396ab17221860fd91d21906db3503c000a131
Steven Roland <steven@mojave.io>
2016-04-21 21:29:25 -0400

    YOU SHALL NOT PASS
    - Established middleware to prevent users that are not authenticated
    from seeing any user endpoint.

--- server.js
+++ server.js
@@ -58,6 +58,25 @@ api.route('/authenticate')
     });
   });
 
+api.use(function(req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+  }
+});
+
 api.route('/users')
   .post(function(req, res) {
     var user = new User();
